40a5223d5c78c78db7c864414708d53bc2ca327e
Tin Maung Zin <tinmaungzin@users.noreply.github.com>
2022-11-07 01:55:52 +0630

    some changes

--- app/Http/Controllers/TypeController.php
+++ app/Http/Controllers/TypeController.php
@@ -5,81 +5,31 @@ namespace App\Http\Controllers;
 use App\Models\Type;
 use Illuminate\Http\Request;
 
-class TypeController extends Controller
+class TypeController extends BasicController
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+    public function __construct(){
+        $type = Type::class;
+        parent::__construct($type);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+
+    public function index(){
+        parent::indexData();
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+    public function store(StaffStoreRequest $request){
+        parent::storeData($request);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Type  $type
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Type $type)
-    {
-        //
+    public function update(StaffUpdateRequest $request, Staff $staff){
+         parent::updateData($request,$staff);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Type  $type
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Type $type)
-    {
-        //
+    public function destroy(Staff $staff){
+         parent::destroyData($staff);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Type  $type
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Type $type)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Type  $type
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Type $type)
-    {
-        //
+    public function search(Request $request){
+         parent::searchData($request);
     }
 }
