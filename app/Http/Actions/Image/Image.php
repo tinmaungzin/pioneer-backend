34492350c641cf23da1a2b50f06539ccca74607c
May <maythazinkhin53747@gmail.com>
2022-11-10 02:13:24 +0630

    image

--- app/Http/Actions/Image/Image.php
+++ app/Http/Actions/Image/Image.php
@@ -4,22 +4,22 @@ namespace App\Http\Actions\Image;
 
 class Image
 {
-    public function save($image)
+    public static function upload($image)
     {
         $image_name = uniqid() . '_' . $image->getClientOriginalName();
         $image_name = str_replace(' ', '', $image_name);
-        $image->move(public_path() . '/storage/images', $image_name);
+        $image->move(storage_path() . '/images/', $image_name);
         return $image_name;
     }
 
     public function delete($image)
     {
-        $file = storage_path('images/' . $image);
+        $file = public_path('/images/' . $image);
         if (file_exists($file)) unlink($file);
     }
 
     public function download($name){
-        return response()->file(storage_path().'/images/'.$name);
+        return response()->file(public_path().'/images/'.$name);
     }
 
 }
