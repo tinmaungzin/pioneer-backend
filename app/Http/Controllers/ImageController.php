1513e93d9c1ccb74f49da98abea091be9e9a3e42
May <maythazinkhin53747@gmail.com>
2022-11-11 10:04:52 +0630

    photo download

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -6,23 +6,33 @@ namespace App\Http\Controllers;
 use App\Http\Actions\Image\Image;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 
 class ImageController extends Controller
 {
     public function downloadImage($name)
     {
-        $path = storage_path('images/' . $name);
-        if (!File::exists($path)) {
-            abort(404);
+        return  $this->getImage('public',$name);
+    }
+
+    public function getImage($path,$file){
+        if($file){
+            $url = $path."/".$file;
+        }else{
+            $url = $path;
         }
+        $path = storage_path("app/") . $url;
+
+        if(!File::exists($path)) abort(404);
+
         $file = File::get($path);
         $type = File::mimeType($path);
 
-        $response = Response::make($file, 200);
-
+        $response = \response($file, 200);
         $response->header("Content-Type", $type);
 
         return $response;
     }
+
 }
