3d43c8fd7e84d28f0a772e19f868a2baaf6c3d42
Tin Maung Zin <tinmaungzin@users.noreply.github.com>
2022-12-27 22:56:56 +0630

    some changes

--- app/Models/Table.php
+++ app/Models/Table.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Table extends Model
 {
     protected $fillable=['name', 'type_id','is_available'];
+    protected $with = ['type'];
 
     public function type()
     {
--- app/Models/Type.php
+++ app/Models/Type.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Type extends Model
 {
-    protected $fillable=['name', 'allowed_people'];
+    protected $fillable=['name', 'allowed_people', 'is_available'];
 
     public function package()
     {
