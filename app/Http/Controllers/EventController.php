e266784fd9fdabac52195cab4a0acdb08f3d8cfd
May <maythazinkhin53747@gmail.com>
2023-10-10 22:25:10 +0700

    event controller

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -57,21 +57,31 @@ class EventController extends BasicController
 
     protected function getTableOfEvent($event)
     {
-        $event_tables = $event->tables->toArray();
-        usort($event_tables, function ($a, $b) {
-            return strnatcmp($a['name'], $b['name']);
-        });
-
-        foreach ($event_tables as $event_table) {
-            $table = (object) $event_table;
+        foreach ($event->tables as $table) {
             $table->price = SetType::where('set_id', $event->set_id)->where('type_id', $table->type_id)->pluck('price')->first();
-            $table->event_table_id = $table->pivot['id'];
-            $table->booking_status = $table->pivot['booking_status'];
+            $table->event_table_id = $table->pivot->id;
+            $table->booking_status = $table->pivot->booking_status;
             $table->allowed_people = Type::where('id', $table->type_id)->pluck('allowed_people')->first();
             $table->packages = Package::where('type_id', $table->type_id)->get();
+            UnsetData($table, ['pivot', 'created_at', 'updated_at']);
+            UnsetData($event, ['set', 'created_at', 'updated_at']);
         }
-        $event->tables = $event_tables;
         return $event;
+//        $event_tables = $event->tables->toArray();
+//        usort($event_tables, function ($a, $b) {
+//            return strnatcmp($a['name'], $b['name']);
+//        });
+//
+//        foreach ($event_tables as $event_table) {
+//            $table = (object) $event_table;
+//            $table->price = SetType::where('set_id', $event->set_id)->where('type_id', $table->type_id)->pluck('price')->first();
+//            $table->event_table_id = $table->pivot['id'];
+//            $table->booking_status = $table->pivot['booking_status'];
+//            $table->allowed_people = Type::where('id', $table->type_id)->pluck('allowed_people')->first();
+//            $table->packages = Package::where('type_id', $table->type_id)->get();
+//        }
+//        $event->tables = $event_tables;
+//        return $event;
     }
 
     public function index()
